2cb428ad53ed7954c72d1791a52cd1bc469e8ad1
Joshua Wiggins <joshua_kar@hotmail.com>
2020-04-07 18:54:32 -0400

    Camera Class completed

---
+++ glAlpha/Camera.cpp
@@ -0,0 +1,83 @@
+#include "Camera.h"
+
+
+Camera::Camera() {
+
+	this->front = glm::vec3(0.0f, 0.0f, -1.0f);
+	this->position = glm::vec3(0.0f, 0.0f, 3.0f);
+	this->movespeedMultiplier = 0.5f;
+	this->sensitivity = 0.5;
+	this->pitch = 20.0f ;
+	this->yaw = 0.0f;
+
+	this->updateCameraPlane();
+}
+
+void Camera::updateCameraPlane() {
+
+	this->front.x = cos(glm::radians(pitch)) * cos(glm::radians(yaw));
+	this->front.y = sin(glm::radians(pitch));
+	this->front.z = cos(glm::radians(pitch)) * sin(glm::radians(yaw));
+	this->front = glm::normalize(this->front);
+
+	this->right = glm::normalize(glm::cross(this->front, glm::vec3(0.0f, 1.0f, 0.0f)));
+	this->up = glm::normalize(glm::cross(this->right, this->front)); 
+
+}
+
+void Camera::updateOrientation(GLfloat mouseXOffset, GLfloat mouseYOffset	) {
+
+	mouseXOffset *= this->sensitivity ;
+	mouseYOffset *= this->sensitivity ;
+
+	this->yaw += mouseXOffset;
+	this->pitch += mouseYOffset;
+
+
+	if (this->pitch > 89.0f)
+		this->pitch = 89.0f;
+	if (this->pitch < -89.0f)
+		this->pitch = -89.0f;
+
+
+	this->updateCameraPlane();
+
+}
+
+
+void Camera::updatePosition(bool* keyPool) {
+
+	if (keyPool[GLFW_KEY_W]) {
+		this->position +=  this->movespeedMultiplier * this->front;
+	}
+
+	if (keyPool[GLFW_KEY_S]) {
+		this->position -= this->movespeedMultiplier *  this->front;
+	}
+
+	if (keyPool[GLFW_KEY_A]) {
+		this->position -= this->movespeedMultiplier *  this->right ;
+	}
+
+	if (keyPool[GLFW_KEY_D]) {
+		this->position += this->movespeedMultiplier *  this->right ;
+	}
+
+}
+
+glm::mat4 Camera::getViewMatrix() {
+
+	return glm::lookAt(this->position, this->front + this->position, this->up);
+
+}
+
+
+glm::vec3 Camera::getCameraWorldPostion() {
+	
+	glm::mat4 view = this->getViewMatrix();
+	glm::vec4 v4WorldSpaceCamera = glm::inverse(view) * glm::vec4(0.0, 0.0, 0.0, 1.0);
+	glm::vec3 v3WorldSpaceCamera = glm::vec3(v4WorldSpaceCamera);
+
+	return v3WorldSpaceCamera;
+
+}
---
+++ glAlpha/Camera.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+
+class Camera
+{
+private:
+	glm::vec3 position;
+	glm::vec3 front;
+	glm::vec3 up;
+	glm::vec3 right;
+	GLfloat pitch;
+	GLfloat yaw;
+	GLfloat movespeedMultiplier;
+	GLfloat sensitivity;
+
+public:
+	Camera();
+	void updateCameraPlane();
+	void updateOrientation(GLfloat mouseXOffset, GLfloat mouseYOffset);
+	void updatePosition(bool* keypool);
+	glm::mat4 getViewMatrix();
+	glm::vec3 getCameraWorldPostion();
+	~Camera();
+};
+
